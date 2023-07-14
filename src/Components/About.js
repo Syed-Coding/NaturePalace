b26ee91adffb4b6bfdb6d50699c5c5371450864c
Syed-Coding <syed2ktym@gmail.com>
2023-07-14 18:05:19 +0530

    second  commit

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useState } from "react";
 import about from "../images/about.jpeg";
 import { Title } from "./Title";
 import { aboutData } from "../utils/data";
